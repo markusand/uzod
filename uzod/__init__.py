525258ba7566be15026c18c3561c7f87939d4a68
Marc Vilella <mvilella.and@gmail.com>
2026-03-21 14:19:08 +0100

    refactor: convert uzod to a proper package

---
+++ uzod/__init__.py
@@ -0,0 +1,5 @@
+"""Schema validator"""
+
+from .uzod import ValidationError, z
+
+__all__ = ["ValidationError", "z"]
---
+++ uzod/__init__.pyi
@@ -0,0 +1,5 @@
+"""Schema validator"""
+
+from .uzod import ValidationError as ValidationError, z as z
+
+__all__ = ["ValidationError", "z"]
